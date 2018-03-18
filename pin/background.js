2c9cbfa166aa288a094730f7bf4f11c9b31e469f
yinm <yinm.cminor9@gmail.com>
2018-03-18 21:48:49 +0900

    ref: https://developer.chrome.com/extensions/examples/api/tabs/pin.zip

---
+++ pin/background.js
@@ -0,0 +1,8 @@
+chrome.commands.onCommand.addListener((command) => {
+  if (command === 'toggle-pin') {
+    chrome.tabs.query({active: true, currentWindow: true}, (tabs) => {
+      const current = tabs[0]
+      chrome.tabs.update(current.id, {'pinned': !current.pinned})
+    })
+  }
+})
