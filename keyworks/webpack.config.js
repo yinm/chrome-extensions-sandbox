7807f9dde6fff2e5fa32efc1db302131bea9d01c
yinm <yinm.cminor9@gmail.com>
2018-07-22 12:06:49 +0900

    Add webpack settings

---
+++ keyworks/webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path')
+
+module.exports = {
+  mode: 'development',
+  entry: {
+    background: './src/background.js',
+    content: './src/content.js'
+  },
+  output: {
+    filename: '[name].js',
+    path: path.join(__dirname, 'dist/assets')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: [['env', { modules: false }]]
+            }
+          }
+        ]
+      }
+    ]
+  }
+}
